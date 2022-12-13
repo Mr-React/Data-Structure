e020baa4d87043d68bf4b889c5a27231c4b4277e
Mr-React <ambureomkar@gmail.com>
2022-12-13 20:02:25 +0530

    Implemented Stack

---
+++ Stack.js
@@ -0,0 +1,69 @@
+class Stack {
+    /**
+     * Stack Structure
+     * @constructor
+     */
+    constructor(){
+        this.item = [];
+    }
+
+    /**
+     * 
+     * @param {any} element - Adds element in stack
+     */
+    push(element){
+        this.item.push(element);
+    }
+
+    /**
+     * Removes element from stack
+     */
+    pop(){
+        return this.item.pop();
+    }
+
+    /**
+     * Returns Top of the stack element
+     * @returns number
+     */
+    peek(){
+        return this.item[this.item.length - 1];
+    }
+
+    /**
+     * Checks Stack is empty or not - returns true if stack is empty otherwise false
+     * @returns boolean
+     */
+    isEmpty(){
+        return this.item.length === 0;
+    }
+
+    /**
+     * Returns the size of Stack
+     * @returns number
+     */
+    size(){
+        return this.item.length;
+    }
+
+    /**
+     * Prints the all elements of stack as string
+     */
+    print(){
+        console.log(this.item.toString());
+    }
+}
+
+const stack = new Stack();
+console.log("Stack is Empty: ",stack.isEmpty());
+
+stack.push(10);
+console.log("Stack is Empty: ", stack.isEmpty());
+stack.push(30);
+stack.push(20);
+
+console.log("Stack size: ", stack.size());
+stack.print();
+
+console.log(stack.pop());
+console.log(stack.peek());
